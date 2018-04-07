3662f20ee395a992de0653107fb5be1cc9a8bba2
bjsmith3185 <bjsmith3185@hotmail.com>
2018-04-07 17:39:01 -0400

    getting closer

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,11 @@
 
 var stage = "start";
 
-var attackPowerUser = ;
-var userHP = ;
+var attackPowerUser; // should these be global
+var userHP;// should these be global
 
-var attackPowerEnemy = ;
-var enemyHP = ;
+//var attackPowerEnemy;// should these be global
+//var enemyHP;// should these be global
 
 
 
@@ -13,66 +13,99 @@ var enemyHP = ;
 
 $(document).ready(function(){
 
+// reset function()-----------------------------------------------
 
+function reset() {  // maybe add a timer
+    stage = "start";
+    $(".character").detach().appendTo("#character-images-top");
+    //alert("Select your player but clicking on the image you choose");
+}
+reset();  // why does this run before the html/css?
+//----------------------------------------------------------------
 
+    
 
-    $(".character").on("click", function() {
-        
-        // want to select the div to be the player, move it to the beginning
-// Start stage Function
-
+          
+// Start stage Function ---------------------------------------------------     
+        // want to select the div to be the player, the remaining will 
+        // be moved to the div below.
+            
+    
+        $(".character").on("click", function() {
         if (stage === "start") {
-        var otherCharacters = $(".character").not(this);
-
-        $(otherCharacters).detach().appendTo("#enemies-to-choose-from");
-
-        console.log(otherCharacters);
-
-        stage = "enemy";
+            var otherCharacters = $(".character").not(this);
+            $(otherCharacters).detach().appendTo("#enemies-to-choose-from");
+                console.log(otherCharacters); // check the location of the enemies in the array
+            // alert("Select you first enemy by clicking on the image you choose");
+            stage = "enemy";
         }
-// Enemy stage Function
+// Enemy stage Function---------------------------------------------------
+        // want to choose the enemy to attack first
 
         else if (stage === "enemy")  {
-        
-        $("#enemies-to-choose-from").on("click", ".character", function() {
+            
+            $("#enemies-to-choose-from").on("click", ".character", function() {
             $(this).detach().appendTo("#enemy-to-battle");
             stage = "attack";
-        })
+            })
         }
-                  
+        // this ends the first .on click function, next is the attack .on click
         });
 
-// Attack stage Function
+// Attack stage Function----------------------------------------------------
 
         // create a stage to determine when both user and enemy have been selected.
         // that stage will control when the button will be active.
 
-    // Need to make this into a function to call with each enemy
+    // **** Need to make this into a function to call with each enemy, 
+    //      but this is still confusing to me  ***
 
         $("button").on("click", function() {
             if(stage === "attack");
             // get HP data from the user's selection
+            var userHP = $(".starting-images .character").attr("data-health-points");
+                userHP = parseInt(userHP);
+                console.log("User's HP = " + userHP);
 
+            // get attackpower from users selection
+            var attackPowerUser = $(".starting-images .character").attr("data-attack-power");
+                attackPowerUser = parseInt(attackPowerUser);
+                console.log("User's attack power =  " + attackPowerUser);
             // get HP data from the enemy
 
+            var enemyHP = $(".enemy .character").attr("data-health-points");
+                enemyHP = parseInt(enemyHP);
+                console.log("Enemy's HP  " + userHP);
+            // get attackpower from enemy
+
+            var attackPowerEnemy = $(".enemy .character").attr("data-attack-power");
+                attackPowerEnemy = parseInt(attackPowerEnemy);
+                console.log("Enemy's attack power  " + userHP);
+            
             // reduces the enemys HP
-            enemyHP -= attackPowerUser;
-            $("health-points-1").text(enemyHP);
+            enemyHP = (enemyHP - attackPowerUser);
+            $(".enemy #health-points").text(enemyHP);
+                console.log("this is the enemies new hp = " + enemyHP);
             // reduces the users HP
-            userHP -= attackPowerEnemy;
-            $("health-points-2").text(userHP);
+            userHP = (userHP - attackPowerEnemy)
+            $(".starting-images #health-points").text(userHP);
+                console.log("this is the user's new hp = " + userHP);
+                console.log("users before attack power = " + attackPowerUser);
+            // increase the user attack poweer
+            attackPowerUser = (attackPowerUser + attackPowerUser);
+                console.log("This is the users attack power = " + attackPowerUser);
 
                 // if statements to determine where to go next
                 if (userHP <= 0) {
                     alert("You Lost....");
-                  //  reset();
+                     reset();
                 } 
                 if (enemyHP <= 0) {
                     alert("You Won");
                     $("#enemy-to-battle").empty();
                     stage = "2ndEnemy";
                 }
-        })
+        });
 
 // 2ndEnemy stage Function
 
@@ -87,29 +120,52 @@ $(document).ready(function(){
 
             $("button").on("click", function() {
                 if(stage === "2ndEnemyAttack");
-                // get HP data from the user's selection
-    
-                // get HP data from the enemy
-    
-                // reduces the enemys HP
-                enemyHP -= attackPowerUser;
-                $("health-points-1").text(enemyHP);
-                // reduces the users HP
-                userHP -= attackPowerEnemy;
-                $("health-points-2").text(userHP);
-    
-                    // if statements to determine where to go next
-                    if (userHP <= 0) {
-                        alert("You Lost....");
-                      //  reset();
-                    } 
-                    if (enemyHP <= 0) {
-                        alert("You Won");
-                        $("#enemy-to-battle").empty();
-                        stage = "3rdEnemy";
-                    }
+
+               // get HP data from the user's selection
+ //           userHP = $(".starting-images .character").attr("data-health-points");
+ //           userHP = parseInt(userHP);
+ //           console.log("User's HP = " + userHP);
+
+        // get attackpower from users selection
+ //       var attackPowerUser = $(".starting-images .character").attr("data-attack-power");
+ //           attackPowerUser = parseInt(attackPowerUser);
+ //           console.log("User's attack power =  " + attackPowerUser);
+
+
+            var enemyHP = $(".enemy .character").attr("data-health-points");
+            enemyHP = parseInt(enemyHP);
+            console.log("Enemy's HP  " + userHP);
+            // get attackpower from enemy
+
+             var attackPowerEnemy = $(".enemy .character").attr("data-attack-power");
+            attackPowerEnemy = parseInt(attackPowerEnemy);
+            console.log("Enemy's attack power  " + userHP);
+        
+            // reduces the enemys HP
+            enemyHP = (enemyHP - attackPowerUser);
+            $(".enemy #health-points").text(enemyHP);
+                console.log("this is the enemies new hp = " + enemyHP);
+            // reduces the users HP
+            userHP = (userHP - attackPowerEnemy)
+            $(".starting-images #health-points").text(userHP);
+                console.log("this is the user's new hp = " + userHP);
+                console.log("users before attack power = " + attackPowerUser);
+            // increase the user attack poweer
+            attackPowerUser = (attackPowerUser + attackPowerUser);
+                console.log("This is the users attack power = " + attackPowerUser);
+
+            // if statements to determine where to go next
+            if (userHP <= 0) {
+                alert("You Lost....");
+                 reset();
+                } 
+            if (enemyHP <= 0) {
+                alert("You Won");
+                $("#enemy-to-battle").empty();
+                 stage = "3rdEnemy";
+                }
             })
-        })
+        });
            
 // 3rdEnemy stage Function
 
@@ -134,6 +190,8 @@ $(document).ready(function(){
                 // reduces the users HP
                 userHP -= attackPowerEnemy;
                 $("health-points-2").text(userHP);
+                // increase the user attack poweer
+                attackPowerUser += attackPowerUser;
     
                     // if statements to determine where to go next
                     if (userHP <= 0) {
@@ -145,11 +203,11 @@ $(document).ready(function(){
                         $("#enemy-to-battle").empty();
                         
                     }
+            // closes attack function
             })
+        // closes 3rdstage attack
         })
-
-
-
+        // closes document.ready
     });
 
 
